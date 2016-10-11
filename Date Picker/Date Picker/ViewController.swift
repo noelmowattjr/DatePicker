8206e3f6ac2593c5d27dd0ed0760fa3bd3156b14
Noel Mowatt <nwmowatt@fullsail.edu>
2016-10-10 20:27:32 -0400

    Added UI Outlets
    Added UI Outlets

--- Date Picker/Date Picker/ViewController.swift
+++ Date Picker/Date Picker/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //--My Outlets
+    @IBOutlet weak var picker: UIDatePicker!
+    @IBOutlet weak var EtiqutteMeansLABELInFrench: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
